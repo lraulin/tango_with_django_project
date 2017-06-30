c9db47a4a5c40ed556924cfce8d9fc7235d33525
Lee Raulin <leeraulin@gmail.com>
2017-06-30 13:31:43 -0400

    finished 17...have duplicated search boxes

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -4,5 +4,24 @@ $('#likes').click(function () {
    $.get('/rango/like/', {category_id: catid}, function(data) {
        $('#like_count').html(data);
        $('#likes').hide();
-   })
+   });
+
+   $('#suggestion').keyup(function () {
+       var query;
+       query = $(this).val();
+       $.get('/rango/suggest/', {suggestion: query}, function(data) {
+           $('#cats').html(data);
+       });
+   });
+
+   $('.rango-add').click(function() {
+       var catid = $(this).attr("data-catid");
+       var url = $(this).attr("data-url");
+       var title = $(this).attr("data-title")
+       var me = $(this)
+       $.get('/rango/add/', {category_id: catid, url: url, title: title}, function(data) {
+           $('#pages').html(data);
+           me.hide();
+       });
+   });
 });
